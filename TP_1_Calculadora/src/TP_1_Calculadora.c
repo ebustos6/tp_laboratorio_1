8c2b7684c803166d632bf4ce4040d5d9e84564ce
ebustos6 <ema.gnr6@gmail.com>
2020-09-13 00:08:36 -0300

    correccion de ultimo minuto

--- TP_1_Calculadora/src/TP_1_Calculadora.c
+++ TP_1_Calculadora/src/TP_1_Calculadora.c
@@ -32,8 +32,10 @@ int main()
     case 1:	if(seguimiento != 0){
     				printf("\nYa se ingreso el primer numero!");
     		}
-    		numero1 = utn_getFloat("Ingrese el primer numero: ");
-    		seguimiento = 1;
+    		else if(seguimiento == 0){
+    				numero1 = utn_getFloat("Ingrese el primer numero: ");
+    			    seguimiento = 1;
+    		}
     		break;
 
     case 2:	if(seguimiento != 1){
@@ -44,8 +46,10 @@ int main()
 					printf("\nYa se ingreso el segundo numero!");
 					}
 			}
-    		numero2 = utn_getFloat("Ingrese el segundo numero: ");
-    		seguimiento = 2;
+    		else if(seguimiento == 1){
+    			numero2 = utn_getFloat("Ingrese el segundo numero: ");
+    			seguimiento = 2;
+    		}
             break;
 
     case 3: if(seguimiento != 2){
@@ -56,13 +60,15 @@ int main()
     				printf("\nYa se calcularon las operaciones!");
     			}
 			}
-    		suma = sumar(numero1, numero2);
-            resta = restar(numero1, numero2);
-            division = dividir(numero1, numero2);
-            multiplicacion = multiplicar(numero1, numero2);
-            factorialA = factorial(numero1);
-            factorialB = factorial(numero2);
-            seguimiento = 3;
+    		else if(seguimiento == 2){
+    			suma = sumar(numero1, numero2);
+    			resta = restar(numero1, numero2);
+    			division = dividir(numero1, numero2);
+    			multiplicacion = multiplicar(numero1, numero2);
+    			factorialA = factorial(numero1);
+    			factorialB = factorial(numero2);
+    			seguimiento = 3;
+    		}
             break;
 
     case 4: if(seguimiento != 3){
@@ -73,17 +79,19 @@ int main()
         			printf("\nFalta calcular las operaciones!");
         			}
     		}
-    		printf("\nEl resultado de A+B es: %.2f", suma);
-            printf("\nEl resultado de A-B es: %.2f", resta);
-            if(numero2 != 0){
-            printf("\nEl resultado de A/B es: %.2f", division);
-            }
-            else{
-                printf("\nNo se puede dividir por 0");
-            }
-            printf("\nEl resultado de A*B es: %.2f", multiplicacion);
-            printf("\nEl factorial de A es: %ld y El factorial de B es: %ld\n", factorialA, factorialB);
-            seguimiento = 0;
+    		else if(seguimiento == 3){
+    			printf("\nEl resultado de A+B es: %.2f", suma);
+    			printf("\nEl resultado de A-B es: %.2f", resta);
+    			if(numero2 != 0){
+    			   printf("\nEl resultado de A/B es: %.2f", division);
+    			   }
+    			else{
+    			   printf("\nNo se puede dividir por 0");
+    			   }
+    			printf("\nEl resultado de A*B es: %.2f", multiplicacion);
+    			printf("\nEl factorial de A es: %ld y El factorial de B es: %ld\n", factorialA, factorialB);
+    			seguimiento = 0;
+    		}
             break;
 
     case 5: respuesta = utn_getChar("\nEsta seguro que desea salir?(s/n): ");
